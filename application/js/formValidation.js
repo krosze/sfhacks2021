56c3e3d95eb5faa039576ec906f8782a5d017c71
Daniela <margolisdaniela@gmail.com>
2021-03-06 10:38:35 -0800

    new files

---
+++ application/js/formValidation.js
@@ -0,0 +1,123 @@
+const populateMapper ={
+
+}
+
+function getSVGIcon(status){
+
+}
+
+function isAlphaNumeric(text){
+    var x, i;
+
+    for (i = 0; i < text.length; i++){
+        x = text.charCodeAt(i);
+        if (!(x > 47 && x < 58) && !(x > 64 && x < 91) && !(x > 96 && x < 123)){
+            return false;
+            }
+    }
+    return true;
+}
+
+function beginWithChar(text){
+    x = text.charCodeAt(0);
+    if (!(x > 64 && x < 91) && !(x > 96 && x < 123)){
+        return false;
+        }
+    return true;
+}
+
+function populateUserValidationDiv(contentDiv){
+
+}
+
+// function validateEmail(){
+
+// }
+
+// function populateEmailValidationDiv(contentDiv){
+
+// }
+
+function containsUpper(text){
+    var x, i;
+    for (i = 0; i < text.length; i++){
+        x = text.charCodeAt(i);
+        if (!(x > 64 && x < 91)){
+            return false;
+            }
+    }
+    return true;
+}
+
+function containsNumber(text){
+    var x, i;
+    for (i = 0; i < text.length; i++){
+        x = text.charCodeAt(i);
+        if (!(x > 47 && x < 58)){
+            return false;
+            }
+    }
+    return true;
+}
+
+function containsSpecial(text){
+    var x, i;
+    for (i = 0; i < text.length; i++){
+        x = text.charCodeAt(i);
+        if (){
+            return false;
+            }
+    }
+    return true;
+}
+
+function populatePasswordValidationDiv(contentDiv){
+
+}
+
+function passwordsMatch(password, cpassword){
+    if(password === cpassword){
+        return true;
+    }
+    return false;
+}
+
+function populateConfirmPasswordValidationDiv(contentDiv){
+
+}
+
+function showValidationDiv(event){
+
+}
+
+function updateUserValidation(event){
+
+}
+
+function updateEmailValidation(event){
+    
+}
+
+function updatePasswordValidation(event){
+
+}
+
+function updateCPasswordValidation(event){
+    
+}
+
+let regFormInputs = document.getElementsByClassName("text-input");
+[...regFormInputs].forEach((input) => {
+    input.addEventListener("focus", showValidationDiv, false);
+    switch(input.getAttribute("id")){
+        case "username":
+            input.addEventListener("input", updateUserValidation, false);
+            break;
+        case "email":
+            input.addEventListener("input", updateEmailValidation, false);
+            break;
+        case "password":
+            input.addEventListener("input", updatePasswordValidation, false);
+            break;
+    }
+})
